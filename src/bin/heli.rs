ff5c834d401f51efe521ea563fe7b630ee24e26d
ZJaume <jzaragoza@prompsit.com>
2024-04-19 08:21:52 +0000

    Set up logger in heli.rs bin

--- src/bin/heli.rs
+++ src/bin/heli.rs
@@ -1,11 +1,13 @@
 use std::io::{self, BufRead};
 
 use log::{info, debug};
+use env_logger::Env;
 
 use heli_otr::identifier::Identifier;
 
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let mut identifier = Identifier::new(String::from("gramdict.ser"),
                                      String::from("wordict.ser"));
 
