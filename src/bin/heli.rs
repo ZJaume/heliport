a15b13de62b31f88afe9a77a933d3d34830c9516
ZJaume <jzaragoza@prompsit.com>
2024-05-29 13:34:30 +0000

    Move cli main loop to a function

--- src/bin/heli.rs
+++ src/bin/heli.rs
@@ -6,14 +6,17 @@ use env_logger::Env;
 use heli_otr::identifier::Identifier;
 
 
-fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let mut identifier = Identifier::new(String::from("gramdict.ser"),
-                                     String::from("wordict.ser"));
-
+fn run(mut identifier: Identifier) {
     let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
         println!("{}", identifier.identify(&line.unwrap()).0);
     }
+}
+
+fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let identifier = Identifier::new(String::from("gramdict.ser"),
+                                     String::from("wordict.ser"));
+    run(identifier);
 }
