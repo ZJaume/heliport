f59c18e680774c7bd86331b593dee4d2ba0606e7
ZJaume <jzaragoza@prompsit.com>
2024-10-28 17:32:38 +0000

    Put binarized models into the wheel

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::copy;
+use std::fs;
 use std::path::PathBuf;
 
 use anyhow::Result;
@@ -11,13 +11,20 @@ use heliport_model::languagemodel::{Model, ModelNgram, OrderNgram};
 fn main() -> Result<(), std::io::Error> {
     let mut model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     model_path.push("LanguageModels");
-    let save_path = PathBuf::from(&env::var("OUT_DIR").unwrap());
+
+    let platlib_path = concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/heliport.data/platlib/heliport",
+    );
+    fs::create_dir_all(&platlib_path)?;
+    let save_path = PathBuf::from(&platlib_path);
 
     // Re-run build script if language models has changed
     println!(
         "cargo:rerun-if-changed={}",
         model_path.display()
     );
+    println!("cargo:rerun-if-changed=build.rs");
 
     //TODO parallelize
     for model_type in OrderNgram::iter() {
@@ -32,7 +39,7 @@ fn main() -> Result<(), std::io::Error> {
         model.save(&filename).unwrap();
     }
     info!("Copying confidence thresholds file");
-    copy(
+    fs::copy(
         model_path.join(Model::CONFIDENCE_FILE),
         save_path.join(Model::CONFIDENCE_FILE),
     ).unwrap();
